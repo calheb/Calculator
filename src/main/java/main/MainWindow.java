982b76d1282cbeb55cacf41b4b80923261b75b04
ctrl-alt-caleb <caleb.blake10@gmail.com>
2022-08-28 09:22:54 -0500

    repo creation

---
+++ src/main/java/main/MainWindow.java
@@ -0,0 +1,16 @@
+package com.calebhebert.calculator;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainWindow extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.show();
+    }
+
+    public void run() {
+        launch();
+    }
+
+}
